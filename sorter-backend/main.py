56c5aeded19ab8d7fdfc649b7c1923d5294934a8
eruuc <eruuc8@gmail.com>
2024-05-22 18:27:32 -0400

    Finished Song Removal
    Added pandas to hold artist data into a dataframe, then added album and song removing functionality

--- sorter-backend/main.py
+++ sorter-backend/main.py
@@ -1,11 +1,7 @@
-import os
-import sys
-import json
 import spotipy
-import webbrowser
+import pandas as pd
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
-from json.decoder import JSONDecodeError
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
@@ -15,76 +11,45 @@ def main():
     print()
 
     artistCatalog = searchArtist()
-    songCount = getSongCount(artistCatalog)
 
-    removeSongs(artistCatalog, songCount)
-    sort()
+    artistCatalog = removeSongs(artistCatalog)
+    
+    sort(artistCatalog)
 
 
 # Song Initialization Works, Need to Figure out Naming Convention
 
 
-def initLists(albums, songCount):
-    # dictionary of albums and their songs with respective info
-    # format:
-    #   artistCatalog key: album name
-    #   artistCatalog[album name][0] = song name
-    #   artistCatalog[album name][1] = score based on sorting
-    #   artistCatalog[album name][2] = song URI
-    #   artistCatalog[album name][3] = song art (based on album art)
-    artistCatalog = dict()
-
-    artistAlbums = []
-    albumList = []
-    albumCount = 1
+def initLists(albums):
+    songNames = []
+    songAlbum = []
+    songScores = []
+    songURIs = []
+    songArt = []
     
-    # grabs album names and info given the passed-in list of albums
     for album in albums:
         albumID = album['id']
         albumArt = album['images'][0]['url']
 
         songs = spotify.album_tracks(albumID)
         songs = songs['items']
-        tempList = []
 
-        # see artistCatalog initialization for meaning of each value in songInfo
         for song in songs:
-            songInfo = []
-            songInfo.append(song['name'])
-            songInfo.append(0)
-            songInfo.append(song['uri'])
-            songInfo.append(albumArt)
+            songNames.append(song['name'])
+            songScores.append(0)
+            songURIs.append(song['uri'])
+            songArt.append(albumArt)
+            songAlbum.append(album['name'])
 
-            tempList.append(songInfo)
-            songCount += 1
-        
-        if (album['name'] in albumList):
-            tempName = album['name'] + " Duplicate " + str(albumCount)
-            albumCount += 1
-            artistCatalog.update({tempName: tempList})
-        else:
-            artistCatalog.update({album['name']: tempList})
-        albumList.append(album['name'])
-    
+    songDict = {'name':songNames,
+                     'album':songAlbum,
+                     'score':songScores,
+                     'uri':songURIs,
+                     'art':songArt}
+
+
+    artistCatalog = pd.DataFrame(songDict)
     return artistCatalog
-
-
-def printSongs(artistCatalog):
-    index = 0
-
-    for album in artistCatalog:
-        print("ALBUM: " + album)
-        for songInfo in artistCatalog[album]:
-            print(str(index) + ": " + songInfo[0])
-            index += 1
-        print()
-    print()
-
-def getSongCount(artistCatalog):
-    songCount = 0
-    for album in artistCatalog:
-        songCount += len(artistCatalog[album])
-    return songCount
 
 def searchArtist():
     searchArtist = input("Search for an Artist: ")
@@ -99,57 +64,59 @@ def searchArtist():
     albums = spotify.artist_albums(artistID)
     albums = albums['items']
 
-    artistCatalog = initLists(albums, 0)
+    artistCatalog = initLists(albums)
 
-    printSongs(artistCatalog)
+    print(artistCatalog.to_string())
 
     return artistCatalog
 
             
-def removeSongs(artistCatalog, songCount):
+def removeSongs(artistCatalog):
     while True:
+        print()
         print("Input an Album Name (exactly) or a song number to remove it from the sorter! Press enter to continue with the sort.")
-        print("Or, type '-1' to view the list of songs!")
         songRemove = input("Album Name or Song Number: ")
         print()
         if songRemove == "":
             break
         else:
             if songRemove.isdigit() or (songRemove.startswith('-') and songRemove[1:].isdigit()):
-                if (int(songRemove) > songCount):
+                if (int(songRemove) >= len(artistCatalog.index)):
                     print("Error! Song number higher than number of songs listed.")
-                elif (int(songRemove) < -1):
+                elif (int(songRemove) < 0):
                     print("Error! Out of range of inputs.")
-                elif (int(songRemove) == -1):
-                    printSongs(artistCatalog)
                 else:
-                    for album in artistCatalog:
-                        for songInfo in artistCatalog[album]:
-                            if songInfo[0] == songRemove:
-                                artistCatalog[album].remove(songInfo)
-                                songCount-=1
-                        
-                    for album in artistCatalog:
-                        print(artistCatalog[album])
-                        if artistCatalog[album] == []:
-                            del artistCatalog[album] 
+                    artistCatalog.drop(index=int(songRemove), inplace=True)
 
                     print("Successfully Removed:", songRemove)
                     print()
-            else:               
-                if songRemove in artistCatalog:
-                    songCount -= len(artistCatalog[songRemove])
-                    del artistCatalog[songRemove]
-                    print()
-                    print("Successfully Removed:", songRemove)
-                    print()
-                else:
+                    artistCatalog.reset_index(drop=True, inplace=True)
+                    print(artistCatalog.to_string())
+            else:
+                mask = artistCatalog['album'] == songRemove
+                newCatalog = artistCatalog[~mask]
+                if newCatalog.equals(artistCatalog):
                     print()
                     print("Could not find:", songRemove)
                     print()
-                
+                else:
+                    print()
+                    print("Successfully Removed:", songRemove)
+                    print()
+                    artistCatalog = newCatalog
+                    artistCatalog.reset_index(drop=True, inplace=True)
+                    print(artistCatalog.to_string())
+    return artistCatalog
 
-def sort():
-    print()
+def sort(artistCatalog):
+    artistCatalog.sort_values(by=['score'])
+    duplicatedSeries = artistCatalog.duplicated(subset=['score'])
+    duplicatedRows = duplicatedSeries.to_list()
+
+    dummyCatalog = artistCatalog
+
+    while (True in duplicatedRows):
+        print()
+
 
 main()
