565f3fb56d10105909a9b2932fb1b2b083463a61
eruuc <eruuc8@gmail.com>
2024-07-10 18:07:40 -0400

    Added Minor Visual Revisions to Borders, Colors, and Sizing
    changed image sizes, font sizes, font types, added circular border and coloring

--- sorter-frontend/song-sorter/src/App.tsx
+++ sorter-frontend/song-sorter/src/App.tsx
@@ -8,18 +8,18 @@ function App() {
     <>
       <div className = 'main-container'>
         <p className = 'title'>Spotify Song Sorter</p>
-          <div className = 'song-choosing-box'>
-              <div className = 'song-display-1'>
-                <img className = 'image' src = {everythingikaboutlove}></img>
-                <p className = 'song-name'>Song 1</p>      
-              </div>
+        <div className = 'song-choosing-box'>
+          <div className = 'song-display-1'>
+            <img className = 'image' src = {everythingikaboutlove}></img>
+            <p className = 'song-name'>Song 1</p>      
+          </div>
 
-              <div className = 'song-display-2'>
-                <img className = 'image' src = {bewitchedgoddessed}></img>
-                <p className = 'song-name'>Song 2</p>
-              </div>
+          <div className = 'song-display-2'>
+            <img className = 'image' src = {bewitchedgoddessed}></img>
+            <p className = 'song-name'>Song 2</p>
           </div>
         </div>
+      </div>
     </>
   )
 }
